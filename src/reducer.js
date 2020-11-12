67011e569a819329e8e80a2eeacadd0ec3df653e
Shiv Patel <shiv.p.patel@outlook.com>
2020-11-12 00:18:41 -0500

    Completed Player page layout

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,8 @@ export const initialState = {
     playlists: [],
     playing: false,
     item:null,
-    token:'BQAuHTBH8wQyCCyUeeSAVe5Z7rkWYHwwhTCdptB6K8Rie_7LBf4F3G3sa250grSYuKVSaXC4JPjHvXYLPfH72u7bTff69MV8_8k2w2TKFN2yVCLjjG2BmdYEaCJqm-_bN34lvc8In2Eb-5aG8tM8ApbP8eWWYK15iD4IJHv5rgoIbW4n',
+    //Remove After Debuging
+    token: null,
 };
 
 const reducer = (state, action) => {
